5ec88fd5eb9fbc0ea35a998865481d99a9804f1d
read <877397708@qq.com>
2017-04-14 16:38:40 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -0,0 +1,30 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/4/14/014.
+ */
+
+public class Province extends DataSupport{
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(){
+        this.id = id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
---
+++ app/src/main/java/com/example/coolweather/util/HttpUtil.java
@@ -0,0 +1,8 @@
+package com.example.coolweather.util;
+
+/**
+ * Created by Administrator on 2017/4/14/014.
+ */
+
+public class HttpUtil {
+}
